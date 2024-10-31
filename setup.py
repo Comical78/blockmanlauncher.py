c03d403224f520fd86dd9358e52d018e66275831
Comical <151361634+ComicalSGT@users.noreply.github.com>
2024-10-31 16:07:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blockmanlauncher.py',                     
+    version='1.0.0',                      
+    author='Spargat Team',                   
+    author_email='blockmanlauncher@gmail.com', 
+    description='Blockman Launcher API on Python',  
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown',
+    url='https://github.com/ComicalSGT/blockmanlauncher.py', 
+    packages=find_packages(),             
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',      
+    ],
+    python_requires='>=3.6',                 
+    install_requires=[                       
+        
+    ],
+) 
